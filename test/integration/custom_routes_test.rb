423edc9f26231104f1cd57e6735719405257a9b9
Shawn Hurd <prince@inner-dimensional.com>
2013-05-29 23:22:32 -0400

    Updated profile page and added scoped updates to it

--- test/integration/custom_routes_test.rb
+++ test/integration/custom_routes_test.rb
@@ -16,4 +16,9 @@ class CustomRoutesTest < ActionDispatch::IntegrationTest
   	get '/register'
   	assert_response :success
   end
+
+  test "that a profile page works" do
+    get '/prince2'
+    assert_response :success
+  end
 end
